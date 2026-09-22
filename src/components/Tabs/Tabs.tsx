7f7be596015271d174cf366cf4ad50eb37afd4c4
Vyacheslav Tsoy <tsoy.slava@gmail.com>
2026-09-23 00:28:43 +0900

    feat: fixed tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -3,13 +3,32 @@ import './Tabs.scss';
 import { Link } from 'react-router-dom';
 
 function Tabs() {
+	const handleClick = (e: any) => {
+		e.preventDefault();
+
+		const allTabs = e.target
+			.closest('ul.tabs')
+			.querySelectorAll('li.tabs__item');
+		const active = e.target.closest('li.tabs__item');
+
+		allTabs.forEach((i: any) => {
+			i.classList.remove('tabs__item--active');
+		});
+
+		active.classList.add('tabs__item--active');
+	};
+
 	return (
 		<ul className="tabs">
 			<li className="tabs__item tabs__item--active">
-				<Link to="#">Your Feed</Link>
+				<Link to="#" onClick={handleClick}>
+					Your Feed
+				</Link>
 			</li>
 			<li className="tabs__item">
-				<Link to="#">Your Feed</Link>
+				<Link to="#" onClick={handleClick}>
+					Your Feed
+				</Link>
 			</li>
 		</ul>
 	);
