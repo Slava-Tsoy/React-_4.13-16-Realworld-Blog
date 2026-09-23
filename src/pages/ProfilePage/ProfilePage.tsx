93c74d9af124ecff12abadd98057cd9301a8b9ca
Vyacheslav Tsoy <tsoy.slava@gmail.com>
2026-09-23 22:53:00 +0900

    fix: pagination on ProfilePage.tsx

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import './ProfilePage.scss';
 
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSearchParams, Link } from 'react-router-dom';
 
 import Panel from '../../components/Panel';
 import Tabs from '../../components/Tabs';
@@ -8,9 +9,9 @@ import Block from '../../components/Block';
 import ArticleList from '../../components/ArticleList';
 import Pagination from '../../components/Pagination';
 import Copyright from '../../components/Copyright';
-// import Preloader from '../../components/Preloader';
+import Preloader from '../../components/Preloader';
 
-// import ErrorPage from '../ErrorPage';
+import ErrorPage from '../ErrorPage';
 
 interface Props {
 	api: any;
@@ -22,6 +23,53 @@ interface Props {
 function ProfilePage(props: Props) {
 	const [articles, articlesUrl] = [props.articles, props.api.articles];
 	const [tags, tagsUrl] = [props.tags, props.api.tags];
+
+	const [searchParams] = useSearchParams();
+	const currentOffset = searchParams.get('offset') || '0';
+	const fetchUrl =
+		props.api.url + articlesUrl + props.api.offset + currentOffset;
+
+	const [items, setItems] = useState(articles);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const control = new AbortController();
+
+		async function getData() {
+			try {
+				setLoading(true);
+
+				await fetch(fetchUrl)
+					.then((res) => res.json())
+					.then((data) => {
+						setItems(data.articles);
+					})
+					.catch((error) => {
+						console.error(error);
+						setLoading(false);
+					});
+			} catch (error: any) {
+				if (error.name !== 'AbortError') {
+					setError(error.message);
+				}
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		getData();
+
+		return () => control.abort();
+	}, [fetchUrl]);
+
+	if (loading) {
+		return <Preloader />;
+	}
+
+	if (error) {
+		return <ErrorPage />;
+	}
 
 	return (
 		<>
@@ -50,7 +98,7 @@ function ProfilePage(props: Props) {
 				<Tabs />
 				<Block items={tags} tagsUrl={tagsUrl} />
 				<ArticleList
-					items={articles}
+					items={items}
 					articlesUrl={articlesUrl}
 					tagsUrl={tagsUrl}
 				/>
